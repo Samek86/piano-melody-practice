5fa988ee44178d01f7b8f5102d6baa3879242dea
Jun <samek86@gmail.com>
2026-09-18 05:09:10 +0900

    chore: hide unfinished beginner songs from the song list

--- src/data/songIndex.ts
+++ src/data/songIndex.ts
@@ -5,48 +5,48 @@ import schoolBell from './songs/beginner/school-bell.json';
 import twinkleTwinkle from './songs/beginner/twinkle-twinkle.json';
 import butterfly from './songs/beginner/butterfly.json';
 import happyBirthday from './songs/beginner/happy-birthday.json';
-import threeBears from './songs/beginner/three-bears.json';
-import mountainRabbit from './songs/beginner/mountain-rabbit.json';
+// import threeBears from './songs/beginner/three-bears.json';
+// import mountainRabbit from './songs/beginner/mountain-rabbit.json';
 import tulip from './songs/beginner/tulip.json';
-import inuNoOmawariSan from './songs/beginner/inu-no-omawari-san.json';
-import donguriKorokoro from './songs/beginner/donguri-korokoro.json';
+// import inuNoOmawariSan from './songs/beginner/inu-no-omawari-san.json';
+// import donguriKorokoro from './songs/beginner/donguri-korokoro.json';
 import yuki from './songs/beginner/yuki.json';
-import usagiToKame from './songs/beginner/usagi-to-kame.json';
-import teruTeruBozu from './songs/beginner/teru-teru-bozu.json';
+// import usagiToKame from './songs/beginner/usagi-to-kame.json';
+// import teruTeruBozu from './songs/beginner/teru-teru-bozu.json';
 import kaeruNoUta from './songs/beginner/kaeru-no-uta.json';
-import roundRound from './songs/beginner/round-round.json';
-import tadpoleFrog from './songs/beginner/tadpole-frog.json';
-import foxFox from './songs/beginner/fox-fox.json';
+// import roundRound from './songs/beginner/round-round.json';
+// import tadpoleFrog from './songs/beginner/tadpole-frog.json';
+// import foxFox from './songs/beginner/fox-fox.json';
 import akatonbo from './songs/beginner/akatonbo.json';
-import umi from './songs/beginner/umi.json';
-import yuyakeKoyake from './songs/beginner/yuyake-koyake.json';
-import furusato from './songs/beginner/furusato.json';
-import momiji from './songs/beginner/momiji.json';
-import islandBaby from './songs/easy/island-baby.json';
+// import umi from './songs/beginner/umi.json';
+// import yuyakeKoyake from './songs/beginner/yuyake-koyake.json';
+// import furusato from './songs/beginner/furusato.json';
+// import momiji from './songs/beginner/momiji.json';
+// import islandBaby from './songs/easy/island-baby.json';
 
 export const allSongs: Song[] = [
   schoolBell,
   twinkleTwinkle,
   butterfly,
   happyBirthday,
-  threeBears,
-  mountainRabbit,
+  // threeBears,
+  // mountainRabbit,
   tulip,
-  inuNoOmawariSan,
-  donguriKorokoro,
+  // inuNoOmawariSan,
+  // donguriKorokoro,
   yuki,
-  usagiToKame,
-  teruTeruBozu,
+  // usagiToKame,
+  // teruTeruBozu,
   kaeruNoUta,
-  roundRound,
-  tadpoleFrog,
-  foxFox,
-  akatonbo,
-  umi,
-  yuyakeKoyake,
-  furusato,
-  momiji,
-  islandBaby
+  // roundRound,
+  // tadpoleFrog,
+  // foxFox,
+  akatonbo
+  // umi,
+  // yuyakeKoyake,
+  // furusato,
+  // momiji,
+  // islandBaby
 ] as Song[];
 
 export const songsByDifficulty = {
